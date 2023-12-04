ecf62a19631a367f428e3fcf8f2c07a75ab6f632
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-12-04 20:45:35 +0000

    fix: adding bcalm as dependency

--- recipes/kissplice/build.sh
+++ recipes/kissplice/build.sh
@@ -6,7 +6,6 @@ export CXXFLAGS="$CXXFLAGS -fcommon -I$PREFIX/include"
 export CFLAGS="$CFLAGS -fcommon -I$PREFIX/include"
 
 # Unfortunately this is not written to pass on appropriate flags
-sed -i.bak "s#fopenmp#fopenmp -I$PREFIX/include#g" CMakeLists.txt
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_LIBRARY_PATH=$PREFIX/lib $DCMAKE_INCLUDE_PATH=$PREFIX/include .
 
 # make commands
